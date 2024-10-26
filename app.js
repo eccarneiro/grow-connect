50145c5a2622d04851ba5f5d82eb5d04393030eb
Emanuel Corrêa <tenseioficial@gmail.com>
2024-10-26 14:14:58 -0300

    add docker

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ import express from 'express';
 import { getCityCords } from './utils/geoUtils.js';
 import { getSheet } from './datasource/sheets.js';
 import 'dotenv/config'
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 const haversineDistance = (lat1, lon1, lat2, lon2) => {
   const toRad = (value) => (value * Math.PI) / 180;
