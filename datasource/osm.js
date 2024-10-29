ddd36e7c4c16fc41dca611002037805a78e31d4b
Emanuel Corrêa <tenseioficial@gmail.com>
2024-10-28 21:59:06 -0300

    feat: adiciona userAgent

--- datasource/osm.js
+++ datasource/osm.js
@@ -2,17 +2,27 @@ import axios from "axios";
 
 export const osmSearch = async (query) => {
     const url = `https://nominatim.openstreetmap.org/search`;
-    const response = await axios.get(url, {
-        params: {
-            q: query,
-            format: 'json',
-            limit: 1
-        },
-    });
 
-    if (response.data.length > 0) {
-        return response.data[0];
-    } else {
-        throw new Error("Local não encontrado.");
+    try {
+        const response = await axios.get(url, {
+            params: {
+                q: query,
+                format: 'json',
+                limit: 1
+            },
+            headers: {
+                // Adiciona um User-Agent personalizado
+                'User-Agent': 'GrowOn/1.0 (tenseioficial@gmail.com)'
+            }
+        });
+
+        if (response.data.length > 0) {
+            return response.data[0];
+        } else {
+            throw new Error("Local não encontrado.");
+        }
+    } catch (error) {
+        console.error("Erro ao buscar dados do OpenStreetMap:", error);
+        throw new Error("Erro ao buscar dados de localização.");
     }
-}
+};
